b3f216be26fe611558beaafdfa71de1c4f9e8e28
hyunjin4270 <hyunn4270@gmail.com>
2024-05-04 16:28:36 +0900

    HashSetex5

---
+++ the_standard_of_java/src/chapter11/p3/ArrayListLinkedListTest.java
@@ -0,0 +1,66 @@
+package chapter11.p3;
+
+import java.util.*;
+
+public class ArrayListLinkedListTest {
+    public static void main(String[] args) {
+        //추가할 데이터의 개수를 고려하여 충분히 잡아야한다.
+        ArrayList al = new ArrayList(2_000_000);
+        LinkedList ll = new LinkedList();
+
+        System.out.println("==순차적으로 추가하기==");
+        System.out.println("ArrayList = " + add1(al));
+        System.out.println("LinkedList = " + add1(ll));
+        System.out.println();
+
+        System.out.println("==중간에 추가하기==");
+        System.out.println("ArrayList = " + add2(al));
+        System.out.println("LinkedList = " + add2(ll));
+        System.out.println();
+
+        System.out.println("==중간에 제거하기==");
+        System.out.println("ArrayList = " + remove2(al));
+        System.out.println("LinkedList = " + remove2(ll));
+        System.out.println();
+
+        System.out.println("==순차적으로 삭제하기==");
+        System.out.println("ArrayList = " + remove1(al));
+        System.out.println("LinkedList = " + remove1(ll));
+        System.out.println();
+
+
+    }
+    static long add1(List list) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            list.add(i+"");
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+    static long add2(List list) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000; i++) {
+            list.add(500, "X");
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+    static long remove1(List list) {
+        long start = System.currentTimeMillis();
+        for (int i = list.size() - 1; i >= 0 ; i--) {
+            list.remove(i);
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+    static long remove2(List list) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000; i++) {
+            list.remove(i);
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+
+}
