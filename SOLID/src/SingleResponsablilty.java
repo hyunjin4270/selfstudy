949b21591d3c653b17abcc19271dab7ab6a772a8
hyunjin4270 <hyunn4270@gmail.com>
2024-05-27 18:19:57 +0900

    123

---
+++ SOLID/src/SingleResponsablilty.java
@@ -0,0 +1,11 @@
+public class SingleResponsablilty {
+    class Player {
+        void move() {
+            /*동작 구현 기능*/
+        }
+        void sound() {
+            /*모든 사운드 구현*/
+        }
+        void 
+    }
+}
